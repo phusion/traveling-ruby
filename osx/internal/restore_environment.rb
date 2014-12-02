d7161a2e4002628775e5ac8e73b47ac890c66d12
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-12-02 17:45:18 +0100

    Work in progress

---
+++ osx/internal/restore_environment.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+# immutable: string
+
+require 'rbconfig'
+
+restorable_envs = ['DYLD_LIBRARY_PATH', 'TERMINFO', 'RUBYOPT', 'RUBYLIB'].freeze
+restorable_envs.each do |name|
+  ENV[name] = ENV["ORIG_#{name}"]
+  ENV.delete("ORIG_#{name}")
+end
+
+# We can't restore these environments now because they'll be used later.
+# We just store the original values so that the program can decide what to do.
+$OVERRIDDEN_ENVIRONMENTS = {
+  'SSL_CERT_DIR'  => ENV['OLD_SSL_CERT_DIR'],
+  'SSL_CERT_FILE' => ENV['OLD_SSL_CERT_FILE']
+}
+
+# Get rid of our custom compilation flags.
+RbConfig::CONFIG["CFLAGS"] = RbConfig::CONFIG["cflags"].dup
+RbConfig::CONFIG["CXXFLAGS"] = RbConfig::CONFIG["cxxflags"].dup
+RbConfig::CONFIG["RUBY_EXEC_PREFIX"] = RbConfig::CONFIG["exec_prefix"].dup
