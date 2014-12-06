31315fdd6e8bdc5a97c4c5af194bc06bdc8093e3
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-12-06 12:30:08 +0100

    Work in progress

--- linux/internal/library.sh
+++ linux/internal/library.sh
@@ -1,7 +1,7 @@
-RESET=`echo 'import sys; sys.stdout.print("\033[0m")'`
-BOLD=`echo 'import sys; sys.stdout.print("\033[1m")'`
-YELLOW=`echo 'import sys; sys.stdout.print("\033[33m")'`
-BLUE_BG=`echo 'import sys; sys.stdout.print("\033[44m")'`
+RESET=`echo 'import sys; sys.stdout.print("\033[0m")' | python`
+BOLD=`echo 'import sys; sys.stdout.print("\033[1m")' | python`
+YELLOW=`echo 'import sys; sys.stdout.print("\033[33m")' | python`
+BLUE_BG=`echo 'import sys; sys.stdout.print("\033[44m")' | python`
 
 function header()
 {
