4fd8c3a570ea14bb4d6681893752e615fd51c145
Dmitry Semernin <dsemernin@gmail.com>
2022-07-09 17:51:25 +0300

    OSX: build supported on x86_64 and arm64 platforms

--- osx/internal/reset_environment.sh
+++ osx/internal/reset_environment.sh
@@ -1,8 +1,10 @@
-export PATH="$SELFDIR/internal/bin":/usr/bin:/bin:/usr/sbin:/sbin
-# FIXME: minimal supported deployment target on ARM64
-export MACOSX_DEPLOYMENT_TARGET=12.1
+# FIXME: minimal supported deployment target on x86_64 and ARM64?
+# NOTE: it looks like this is used only for building PostgreSQL
+# export MACOSX_DEPLOYMENT_TARGET=12.1
 # FIXME: a full list of deployment targets on ARM64
-export MACOSX_COMPATIBLE_DEPLOYMENT_TARGETS="10.14 10.15 11.0 11.1 11.2 11.3 11.4 11.5 11.6 12.0 12.1"
+# FIXME: not used at all?
+# export MACOSX_COMPATIBLE_DEPLOYMENT_TARGETS="10.14 10.15 11.0 11.1 11.2 11.3 11.4 11.5 11.6 12.0 12.1"
+export PATH="$SELFDIR/internal/bin":/usr/bin:/bin:/usr/sbin:/sbin
 export CC="$SELFDIR/internal/bin/cc"
 export CXX="$SELFDIR/internal/bin/c++"
 unset DYLD_LIBRARY_PATH
