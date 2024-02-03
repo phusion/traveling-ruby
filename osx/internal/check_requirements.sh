8276847f91d5bfca6bb8b518ffd8261d09102876
Yousaf Nabi <you@saf.dev>
2024-02-03 23:08:51 +0000

    ci: arm64 gha support / cirrus download runtime paths

--- osx/internal/check_requirements.sh
+++ osx/internal/check_requirements.sh
@@ -9,6 +9,8 @@ elif [[ "$XCODEPATH" == "/Applications/Xcode.app/Contents/Developer" ]]; then
   XCODEPATH="$XCODEPATH/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 elif [[ "$XCODEPATH" == "/Applications/Xcode_14.2.app/Contents/Developer" ]]; then
   XCODEPATH="$XCODEPATH/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+elif [[ "$XCODEPATH" == "/Applications/Xcode_14.3.1app/Contents/Developer" ]]; then
+  XCODEPATH="$XCODEPATH/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 else
   echo "*** ERROR: unknown developer path \"$XCODEPATH\""
   exit 1
