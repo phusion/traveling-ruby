e4eab0160408e40ba1f1af17d18803ae041f05c7
Yousaf Nabi <you@saf.dev>
2024-02-04 18:03:46 +0000

    ci: use uploaded osx gha runtime to speed up runs

--- scripts/download-osx-runtime.sh
+++ scripts/download-osx-runtime.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "downloading pre-packaged runtime"
-curl -L -o $PLATFORM/runtime.tar.gz https://github.com/YOU54F/traveling-ruby/releases/download/rel-20240116/osx-runtime-$ARCHITECTURES.tar.gz
+curl -L -o $PLATFORM/runtime.tar.gz https://github.com/YOU54F/traveling-ruby/releases/download/rel-20240201/osx-runtime-$ARCHITECTURES-gha.tar.gz
 echo "unpacking pre-packaged runtime"
 mkdir -p $PLATFORM/runtime
 [[ -f "$PLATFORM/runtime.tar.gz" ]] && cd $PLATFORM && tar -xzf ../$PLATFORM/runtime.tar.gz && rm -rf ../$PLATFORM/runtime.tar.gz && echo "unpacked osx-runtime-$ARCHITECTURES.tar.gz" && ls -al ../$PLATFORM/runtime
