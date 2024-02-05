90bfc7e941d72b8da46a12f2d44c98ca5a256449
Yousaf Nabi <you@saf.dev>
2024-02-05 22:34:48 +0000

    ci(matrix): test multiple ruby versions inc openssl 1.1.1

--- shared/package.sh
+++ shared/package.sh
@@ -5,7 +5,7 @@ SELFDIR=`dirname "$0"`
 SELFDIR=`cd "$SELFDIR" && pwd`
 source "$SELFDIR/library.sh"
 BUNDLER_VERSION=`cat "$SELFDIR/../BUNDLER_VERSION.txt"`
-if [[ "$RUBY_VERSION" < "3.0" ]]; then
+if [[ "$RUBY_VERSIONS" < "3.0.0" ]]; then
     BUNDLER_VERSION="2.4.22"
 fi
 BUILD_OUTPUT_DIR=
